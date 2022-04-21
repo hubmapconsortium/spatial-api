ea34b1164cfefe5592b1c65f41c78d9faed75354
ChuckKollar <chuck@kollar.com>
2022-04-21 14:18:53 -0400

    Did some refactoring and added scripts for testing

---
+++ scripts/search_hubmap_id.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+set -u
+
+curl 'localhost:5001/search/hubmap_id/HBM634.MMGK.572/radius/0.25'
--- server/spatialapi/search_hubmap_id/__init__.py
+++ server/spatialapi/search_hubmap_id/__init__.py
@@ -11,6 +11,7 @@ search_hubmap_id_to_radius_blueprint = Blueprint('spacial', __name__, template_f
 
 @search_hubmap_id_to_radius_blueprint.route('/search/hubmap_id/<id>/radius/<r>', methods=['GET'])
 def search_hubmap_id_to_radius(id, r):
+    logger.info(f'search_hubmap_id_to_radius: GET /search/hubmap_id/{id}/radius/{r}')
     #replace by the correct way to check token validity.
     authenticated = session.get('is_authenticated')
     if not authenticated:
@@ -18,7 +19,7 @@ def search_hubmap_id_to_radius(id, r):
         redirect_url = current_app.config['FLASK_APP_BASE_URI'].rstrip('/') + '/login'
         return redirect(redirect_url)
     results = g.spatial_manager.find_within_radius_at_hubmap_id(r, id)
-    logger.info(f'search_hubmap_id({id},{r}): {results}')
+    logger.info(f'search_hubmap_id_to_radius; find_within_radius_at_hubmap_id({id},{r}): {results}')
     response = make_response(jsonify(ids=results), 200)
     response.headers["Content-Type"] = "application/json"
     return response
