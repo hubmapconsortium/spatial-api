c87eb6059868b543708ed25623794b554c38667a
ChuckKollar <chuck@kollar.com>
2022-04-22 16:06:42 -0400

    Fixed port in script

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -6,4 +6,4 @@ POSTGRES_USER=spatial
 POSTGRES_DB=spatial
 
 echo "Running test queries..."
-psql -v ON_ERROR_STOP=1 -h localhost -p 5433 -U $POSTGRES_USER -d $POSTGRES_DB -a -f ./db/run_test.sql
+psql -v ON_ERROR_STOP=1 -h localhost -p 5432 -U $POSTGRES_USER -d $POSTGRES_DB -a -f ./db/run_test.sql
