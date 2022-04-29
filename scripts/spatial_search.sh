b4c92b4564bdb4bc44af29e850122d610782c17e
ChuckKollar <chuck@kollar.com>
2022-04-29 16:41:17 -0400

    Added some request validation

--- scripts/spatial_search.sh
+++ scripts/spatial_search.sh
@@ -2,7 +2,7 @@
 set -e
 set -u
 
-curl -si -H 'Content-Type: application/json' -d '{"target": "VHMale", "radius": 10, "x":5, "y": 5, "z":5}' -X POST 'http://localhost:5001/spatial-search'
+curl -si -H 'Content-Type: application/json' -d '{"target": "VHMale", "radius": '5.76456', "x":5.234, "y": 5, "z": 5}' -X POST 'http://localhost:5001/spatial-search'
 
 
 
--- server/spatialapi/spatial_search/__init__.py
+++ server/spatialapi/spatial_search/__init__.py
@@ -16,17 +16,26 @@ spatial_search_blueprint = Blueprint('spatial_search_blueprint', __name__)
 def spatial_search():
     request_dict: dict = request.get_json()
     logger.info(f'spatial_search: POST /spatial-search {request_dict}')
-    required_request_keys: tuple = ("target", "radius", 'x', 'y', 'z')
-    if not all(key in request_dict for key in required_request_keys):
-        abort(json_error(f'Request Body must have the following required attributes {required_request_keys}', 400))
+    request_validation(request_dict)
 
     config = configparser.ConfigParser()
     app_properties: str = 'resources/app.properties'
     logger.info(f'spatial_search: Reading properties file: {app_properties}')
     config.read(app_properties)
     spatial_manager = SpatialManager(config)
-    
+
     results = {}
     response = make_response(jsonify(hubmap_ids=results), 200)
     response.headers["Content-Type"] = "application/json"
     return response
+
+def request_validation(request_dict: dict) -> None:
+    required_request_keys: tuple = ("target", "radius", 'x', 'y', 'z')
+    if not all(key in request_dict for key in required_request_keys):
+        abort(json_error(f'Request Body: Must have the following required attributes {required_request_keys}', 400))
+    int_instances_keys: tuple = ("radius", 'x', 'y', 'z')
+    if not all(isinstance(value, (int, float)) for value in [request_dict[k] for k in int_instances_keys]):
+        abort(json_error(f'Request Body: The following attributes {int_instances_keys} must have numeric values', 400))
+    target_values: list = ['VHMale', 'VHFemale']
+    if not request_dict['target'] in target_values:
+        abort(json_error(f'Request Body: The attribute "target" must be one of: {", ".join(target_values)}', 400))
