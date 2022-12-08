18dc7232a7656486389ed9c745be6f27ed3d77a8
ChuckKollar <chuck@kollar.com>
2022-12-08 13:23:00 -0500

    Added database table rebuild to db_rebuild.sh

--- scripts/db_rebuild.sh
+++ scripts/db_rebuild.sh
@@ -4,29 +4,37 @@ set -u
 
 SCHEME_HOST_PORT=https://spatial-api.dev.hubmapconsortium.org
 # SCHEME_HOST_PORT=http://localhost:5001
+DATABASE_HOST_PORT=18.205.215.12:5432
 BEARER_TOKEN=
 INCREMENTAL_REINDEX=0
+RECREATE_DATABASE_TABLES=0
 VERBOSE=
-
-# To reload the database tables on dev...
-# $ psql -h 18.205.215.12 -p 5432 -d spatial -U spatial -f db/initdb.d/initdb.sql
 
 usage()
 {
-  echo "Usage: $0 [-H SCHEME_HOST_PORT] [-t BEARER_TOKEN] [-v] [-h]"
-  echo " -H Scheme, host, and port (default $SCHEME_HOST_PORT)"
-  echo " -t BEARER_TOKEN (no default)"
-  echo " -i Incremental Reindex samples"
+  echo "Usage: $0 [-H SCHEME_HOST_PORT] [-D DATABASE_HOST_PORT] [-t BEARER_TOKEN] [-i] [-r] [-v] [-h]"
+  echo " -H Scheme, host, and port of spatial-api server (${SCHEME_HOST_PORT})"
+  echo " -D HOST:PORT Database Host and Port separated by a colon ':' (${DATABASE_HOST_PORT})"
+  echo " -t BEARER_TOKEN (see text below)"
+  echo " -i Incremental Reindex samples that are older than last processed"
+  echo " -r Delete and then recreate Databases Tables from 'db/initdb.d/initdb.sql'"
   echo " -v Verbose"
   echo " -h Help"
+  echo
+  echo "To get the BEARER_TOKEN, login through the UI (https://ingest.hubmapconsortium.org/) to get the credentials"
+  echo "In Firefox open 'Tools > Browser Tools > Web Developer Tools'."
+  echo "Click on "Storage" then the dropdown for "Local Storage" and then the url,"
+  echo "and use the "groups_token" from the returned information."
   exit 2
 }
 
-while getopts 'H:t:vih' arg; do
+while getopts 'H:D:t:irvh' arg; do
   case $arg in
     H) SCHEME_HOST_PORT=$OPTARG ;;
+    D) DATABASE_HOST_PORT=$OPTARG ;;
     t) BEARER_TOKEN=$OPTARG ;;
     i) INCREMENTAL_REINDEX=1 ;;
+    r) RECREATE_DATABASE_TABLES=1 ;;
     v) VERBOSE='--verbose' ;;
     h|?) usage ;;
   esac
@@ -35,18 +43,52 @@ done
 shift $((OPTIND-1))
 
 echo
-echo "Scheme, host, and port: ${SCHEME_HOST_PORT}"
+echo "spatial-api: Scheme, host, and port: ${SCHEME_HOST_PORT}"
+echo "db: host, port: ${DATABASE_HOST_PORT}"
 echo "Bearer Token: ${BEARER_TOKEN}"
 echo "Incremental Reindex: ${INCREMENTAL_REINDEX}"
+echo "Recreate database tables: ${RECREATE_DATABASE_TABLES}"
+echo
 
-if [[ INCREMENTAL_REINDEX -eq 1 ]]; then
+IFS=':'; arr_db_host_port=($DATABASE_HOST_PORT); unset IFS;
+if [[ ${#arr_db_host_port[@]} -ne 2 ]]; then
+  echo
+  echo "ERROR: -D HOST:PORT Both host and port must be specified separated by a ':'."
+  echo
+  usage
+  exit 1
+fi
+db_host=${arr_db_host_port[0]}
+db_port=${arr_db_host_port[1]}
+
+if [[ $RECREATE_DATABASE_TABLES -eq 1 ]] && [[ $INCREMENTAL_REINDEX -eq 1 ]]; then
+  echo
+  echo "ERROR: You can only recreate the database tables if you are doing a full reindex and not an incremental one."
+  echo
+  usage
+  exit 1
+fi
+
+if [[ $INCREMENTAL_REINDEX -eq 1 ]]; then
   echo
   echo ">>> Extract cell_type_counts for modified samples..."
+  exit 1
   curl $VERBOSE -X PUT -si "${SCHEME_HOST_PORT}/samples/incremental-reindex" \
     -H "Authorization: Bearer $BEARER_TOKEN"
   echo
   echo "Done!"
   exit 0
+fi
+
+if [[ $RECREATE_DATABASE_TABLES -eq 1 ]]; then
+  if [[ `which psql > /dev/null 2>&1; echo $?` -ne 0 ]] ; then
+    echo "Installing postgresql using brew..."
+    brew install postgresql
+  fi
+  echo
+  echo ">>> Reloading database tables; dbhost: ${db_host}; db_port: ${db_port}"
+  echo "Look in the resources/app.properties file for the [postgresql] Passowrd."
+  psql -h ${db_host} -p ${db_port} -d spatial -U spatial -f db/initdb.d/initdb.sql
 fi
 
 echo
@@ -59,7 +101,7 @@ curl $VERBOSE -X PUT -si "${SCHEME_HOST_PORT}/rebuild-annotation-details" \
  -H "Authorization: Bearer $BEARER_TOKEN"
 
 echo
-echo ">>> Extract cell_type_counts for all samples..."
+echo ">>> Reindexing all samples..."
 curl $VERBOSE -X PUT -si "${SCHEME_HOST_PORT}/samples/reindex-all" \
  -H "Authorization: Bearer $BEARER_TOKEN"
 
